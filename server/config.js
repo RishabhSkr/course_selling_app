093a76becbb2cfc9a1b6b6c567303b8b0121616f
RishabhSkr <iamrishab786@gmail.com>
2024-11-28 01:39:14 +0530

    DB connection set to locally changed to atlas string

--- server/config.js
+++ server/config.js
@@ -4,6 +4,6 @@ dotenv.config();
 
   export const JWT_SECRET = process.env.JWT_SECRET;
   export const JWT_SECRET_ADMIN = process.env.JWT_SECRET_ADMIN;
-  export const DB_URI = 'mongodb://localhost:27017/courses';
+  export const DB_URI = 'mongodb+srv://admin-Rishabh:test123@cluster0.jpn1sz9.mongodb.net/courses';
   export const JWT_SECRET_SUPER = 'SuperMac'
   
